a646359a371df2eca2110e5c764d52cb53363922
mkawai <mkawai@aol.jp>
2016-01-03 17:59:04 +0900

    Modified UserClass.

--- Modules/userclass.php
+++ Modules/userclass.php
@@ -7,10 +7,21 @@
 
 // Get user's information. 
 class UserClass{
-    public $user_id = $_GET['id'];
-    public $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    public $hostaddress = $_SERVER['REMOTE_HOST'];
-    public $ipaddress = $_SERVER['REMOTE_ADDR'];
+    public $id;
+    public $agent;
+    public $hostaddress;
+    public $ipaddress;
+    public function show(){
+        echo "ID:{$this->id}<br>Agent:{$this->agent}<br>Host:{$this->hostaddress}<br>IP:{$this->ipaddress}<br>";
+    }
 }
 
+
+// Get User's env info
+    $user = new UserClass;
+    $user->id = $_GET['id'];
+    $user->agent = $_SERVER['HTTP_USER_AGENT'];
+    $user->hostaddress = $_SERVER['REMOTE_HOST'];
+    $user->ipaddress = $_SERVER['REMOTE_ADDR'];
+    $user->show();
 
