beff167ec73db97110c3ae72b39de4c75cc1b552
mkawai <mkawai@aol.jp>
2016-01-04 18:12:09 +0900

    Modified

--- Modules/userclass.php
+++ Modules/userclass.php
@@ -11,15 +11,14 @@ class UserClass{
     private $agent;
     private $hostaddress;
     private $ipaddress;
+
     public function __construct(){
     // Get User's env info
         $this->id = $_GET['id'];
         $this->agent = $_SERVER['HTTP_USER_AGENT'];
         $this->hostaddress = $_SERVER['REMOTE_HOST'];
         $this->ipaddress = $_SERVER['REMOTE_ADDR'];
-        echo "ID:{$this->id}<br>Agent:{$this->agent}<br>Host:{$this->hostaddress}<br>IP:{$this->ipaddress}<br>";
+//        echo "ID:{$this->id}<br>Agent:{$this->agent}<br>Host:{$this->hostaddress}<br>IP:{$this->ipaddress}<br>";
     }
 }
 
-
-new UserClass();
