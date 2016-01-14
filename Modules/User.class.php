eea814f7c01e45176861ea1ba5edfa4a6f840ace
mkawai <mkawai@aol.jp>
2016-01-14 10:10:07 +0900

    Modified UserDatabaseClass

--- Modules/User.class.php
+++ Modules/User.class.php
@@ -50,39 +50,46 @@
 /***********************/
 /*  データベースクラス  */
 /***********************/
-	class Database{
-		public function login($sql,$password){
-			try {
-			    $pdo = new PDO(
-			        sprintf('mysql:dbname=%s;host=%s;charset=%s',DBNAME,DBHOST,DBCHARSET),
-			        DBUSER,
-			        DBPASS,
-			        array(
-			            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			            PDO::ATTR_EMULATE_PREPARES => false,
-			        )
-			    );
-			    $pdo->beginTransaction();
-			    // $sql = 'SELECT * FROM eb_users WHERE id = :id';
-			    $stmt = $pdo->prepare($sql);
-			    $stmt->bindParam(':id', $id);
-			    $stmt->execute();
-			    $count = $stmt->rowCount();
-			    if($count == 1){
-			    	$_SESSION['id'] = $id;
-			    	echo "Logged in";
-			    	return true;
-			    }else{
-			    	echo "Couldn't find your ID";
-			    	return false;
-			    }
-		
-			} catch (Exception $e) {
-			    //var_dump($e->getMessage());
-			    $export['code'] = 500;
-			    //rollback when failed.
-			    $pdo->rollBack();
+	class UserDatabase{
+		public function __construct($sql){
+			private $id;
+			$id = $_SESSION['id'];
+				if(isset($id)){
+				try {
+				    $pdo = new PDO(
+				        sprintf('mysql:dbname=%s;host=%s;charset=%s',DBNAME,DBHOST,DBCHARSET),
+				        DBUSER,
+				        DBPASS,
+				        array(
+				            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				            PDO::ATTR_EMULATE_PREPARES => false,
+				        )
+				    );
+				    $pdo->beginTransaction();
+				    $sql_search_id = 'SELECT * FROM eb_users WHERE id = :id';
+				    $stmt = $pdo->prepare($sql_search_id);
+				    $stmt->bindParam(':id', $id);
+				    $stmt->execute();
+				    $count = $stmt->rowCount();
+			    	//セッションIDがあれば引数のSQL文を実行する
+				    if($count == 1){
+					    $stmt = $pdo->prepare($sql);
+					    $stmt->execute();
+				    	// echo "Logged in";
+				    	return true;
+				    }else{
+				    	// echo "Couldn't find your ID";
+				    	return false;
+				    }
+			
+				} catch (Exception $e) {
+				    //var_dump($e->getMessage());
+				    $export['code'] = 500;
+				    //rollback when failed.
+				    $pdo->rollBack();
+				}
 			}
+		}else{
+			exit;
 		}
-
 	}
