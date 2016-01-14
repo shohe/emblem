88f21116c37e878bfcd4e93b481a2618e61ca63a
mkawai <mkawai@aol.jp>
2016-01-14 11:40:14 +0900

    Class:Ticket,Store,Database + Modified

--- Modules/User.class.php
+++ Modules/User.class.php
@@ -6,8 +6,7 @@
 
 	require_once 'config.php';
 
-
-
+	echo "test";
 /***********************/
 /*    ユーザークラス    */
 /***********************/
@@ -19,25 +18,26 @@
 
 		function __construct(){
 			$this->id = (isset($_GET['id'])) ? $_GET['id'] : null;
-	        $this->agent = $_SERVER['HTTP_USER_AGENT'];
-	        $this->hostaddress = $_SERVER['REMOTE_HOST'];
-	        $this->ipaddress = $_SERVER['REMOTE_ADDR'];
+			$this->agent = $_SERVER['HTTP_USER_AGENT'];
+			$this->hostaddress = $_SERVER['REMOTE_HOST'];
+			$this->ipaddress = $_SERVER['REMOTE_ADDR'];
 		}
 
 		public function setSession() {
 			session_start();
 			$_SESSION['id'] = $this->id;
-	        $_SESSION['agent'] = $this->agent;
+			$_SESSION['agent'] = $this->agent;
 			$_SESSION['hostaddress'] = $this->hostaddress;
 			$_SESSION['ipaddress'] = $this->ipaddress;
 		}
 
-		/* ログアウト（セッション削除） */
+		/* ログアウト（セッション削除）*/
 		public function destroySession(){
 			if($_SESSION['id']){
 				$_SESSION = array();
 				session_destroy();
 				echo "Logged out.";
+			}
 		}
 
 		/* ハッシュ化 */
@@ -48,48 +48,51 @@
 
 
 /***********************/
+/*    チケットクラス    */
+/***********************/
+	class Ticket{
+
+	}
+
+
+/***********************/
+/*     ストアクラス     */
+/***********************/
+
+	class Store{
+
+	}
+
+
+
+/***********************/
 /*  データベースクラス  */
 /***********************/
-	class UserDatabase{
-		public function __construct($sql){
-			private $id;
-			$id = $_SESSION['id'];
-				if(isset($id)){
+
+	class Database{
+			public function database($sql){
 				try {
 				    $pdo = new PDO(
 				        sprintf('mysql:dbname=%s;host=%s;charset=%s',DBNAME,DBHOST,DBCHARSET),
 				        DBUSER,
 				        DBPASS,
-				        array(
-				            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-				            PDO::ATTR_EMULATE_PREPARES => false,
-				        )
-				    );
+			    	    array(
+			        	    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			            	PDO::ATTR_EMULATE_PREPARES => false,
+			        	)
+			    	);
 				    $pdo->beginTransaction();
-				    $sql_search_id = 'SELECT * FROM eb_users WHERE id = :id';
-				    $stmt = $pdo->prepare($sql_search_id);
-				    $stmt->bindParam(':id', $id);
+				    $stmt = $pdo->prepare($sql);
 				    $stmt->execute();
-				    $count = $stmt->rowCount();
-			    	//セッションIDがあれば引数のSQL文を実行する
-				    if($count == 1){
-					    $stmt = $pdo->prepare($sql);
-					    $stmt->execute();
-				    	// echo "Logged in";
-				    	return true;
-				    }else{
-				    	// echo "Couldn't find your ID";
-				    	return false;
-				    }
-			
-				} catch (Exception $e) {
+				    $result = $stmt->fetchAll();
+				    return $result;
+				    
+				}catch (Exception $e) {
 				    //var_dump($e->getMessage());
 				    $export['code'] = 500;
 				    //rollback when failed.
-				    $pdo->rollBack();
+			    	$pdo->rollBack();
 				}
 			}
-		}else{
-			exit;
 		}
 	}
